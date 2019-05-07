59601b3a61aac38eae3bb2b8e66b25ed24c8ea8f
NoName <43968654+385433799@users.noreply.github.com>
2019-05-06 17:07:55 -0700

    Update pca_code.R

--- pca_code.R
+++ pca_code.R
@@ -1,12 +1,6 @@
 setwd("/Users/siyuanxue/Desktop/capstone/pre-PCA/pca")
-cpm_data <- read.delim("movie_cleaned3.csv", sep=',', check.names=FALSE, stringsAsFactors=FALSE)
+cpm_data <- read.delim("cpm.csv", sep=',', check.names=FALSE, stringsAsFactors=FALSE)
 #this place can choose whether use transform or not
-cpm_data$Title = NULL
-cpm_data$Votes = NULL
-cpm_data$Year = NULL
-cpm_data$Gross_Earning_in_Mil = NULL
-cpm_data$Rating = NULL
-cpm_data[,1:6] = NULL
 pca <- prcomp(t(cpm_data),scale=TRUE)
 plot(pca$x[,1],pca$x[,2])
 pca.var <- pca$sdev*2
